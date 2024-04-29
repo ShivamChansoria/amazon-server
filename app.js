2914afb154c9c159b8be371583f6f574fccae0f6
ShivamChansoria <chansoriyashivam@gmail.com>
2024-04-29 15:59:40 +0530

    minor-updates

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) =>{
+   res.send("<h1>Server is now working Live!!!</h1>")
+})
 
 
 app.use(cors());
